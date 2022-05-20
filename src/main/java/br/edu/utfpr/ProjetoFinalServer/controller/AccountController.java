67f15a5cb84d0070ec102f27c210dfd3a1408ada
rlraul <raul.cardoso@outlook.com.br>
2022-05-19 23:48:52 -0300

    Criada a classe de controller generica;

--- src/main/java/br/edu/utfpr/ProjetoFinalServer/controller/AccountController.java
+++ src/main/java/br/edu/utfpr/ProjetoFinalServer/controller/AccountController.java
@@ -1,15 +1,22 @@
 package br.edu.utfpr.ProjetoFinalServer.controller;
 
+import br.edu.utfpr.ProjetoFinalServer.model.Account;
 import br.edu.utfpr.ProjetoFinalServer.service.AccountService;
+import br.edu.utfpr.ProjetoFinalServer.service.impl.CrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("account")
-public class AccountController {
+public class AccountController extends CrudController<Account, Long>{
 
     @Autowired
     private AccountService accountService;
+
+    @Override
+    protected CrudService<Account, Long> getService() {
+        return this.accountService;
+    }
 
 }
